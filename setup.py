4506c07902c043e08b9d8049917dd792538f5076
Matt Johnson <mdj2@pdx.edu>
2014-07-16 16:11:19 -0700

    version++

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="elasticmodels",
-    version="0.0.3",
+    version="0.0.4",
     url='https://github.com/PSU-OIT-ARC/elasticmodels',
     author='Matt Johnson',
     author_email='mdj2@pdx.edu',
