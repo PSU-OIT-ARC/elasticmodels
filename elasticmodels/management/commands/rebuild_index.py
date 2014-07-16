747ae753ef031e6d23fc3d7f1ac4d6e25e13309a
Matt Johnson <mdj2@pdx.edu>
2014-07-16 16:10:54 -0700

    Pep8; Python3 compatibility

--- elasticmodels/management/commands/rebuild_index.py
+++ elasticmodels/management/commands/rebuild_index.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import elasticsearch
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
@@ -10,9 +11,7 @@ class Command(BaseCommand):
         clear_index()
 
         for model_class, index in index_registry.items():
-            
-            model = index.model            
-            
+            model = index.model
             es().indices.put_mapping(
                 index=settings.ELASTIC_SEARCH_INDEX,
                 doc_type=index.doc_type,
@@ -25,7 +24,7 @@ class Command(BaseCommand):
             #for obj in model.objects.all():
             for obj in index.get_queryset():
             #for obj in model.objects.filter(**(index.filter_params)):
-                print "indexing %s pk = %d" % (obj.__class__.__name__, obj.pk)
+                print("indexing %s pk = %d" % (obj.__class__.__name__, obj.pk))
                 # TODO use a bulk update for this
                 make_searchable(obj, refresh=False)
             es().indices.refresh(index=settings.ELASTIC_SEARCH_INDEX)
--- elasticmodels/models.py
+++ elasticmodels/models.py
@@ -2,16 +2,13 @@
 Load all the search_indexes modules from all the apps. This has the side effect
 of populating the index_registry in search/__init__.py
 """
-from . import make_searchable
 from django.db.models.loading import get_apps
+from . import make_searchable
 
 apps = get_apps()
 for app in apps:
     try:
-
         __import__(app.__package__ + ".search_indexes")
-        print "%s imported!" % str(app)
-
-    except (ImportError, TypeError):
+    except (ImportError, TypeError) as e:
         # The package was None, or the search_indexes file doesn't exist
         pass
